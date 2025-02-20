f7ac1508ea2458ae8489ba57ab93c8d015cf91c3
imnxtamess <davide.morisco.work@gmail.com>
2025-02-20 16:27:31 +0100

    add a message to display guess amount and numbers guessed

--- js/script.js
+++ js/script.js
@@ -72,19 +72,20 @@ console.log(ranNumList);
 formEl.addEventListener('submit', function (e) {
   e.preventDefault()
   const inputArray = [];
-  inputArray.push(inputNum1.value, inputNum2.value, inputNum3.value, inputNum4.value, inputNum5.value)
+  inputArray.push(inputNum1.value, inputNum2.value, inputNum3.value, inputNum4.value, inputNum5.value) // pushes user input numbers into the input array
   console.log(inputArray);
   let guessed_count = 0
   let guessed_list = []
   for (let i = 0; i < inputArray.length; i++) {
     thisNum = inputArray[i]
     if (ranNumList.includes(thisNum)) {
-      guessed_count += 1
-      guessed_list.push(thisNum)
+      guessed_count += 1 // adds 1 to 0 each time a number is correctly guessed by checking the random numbers list
+      guessed_list.push(thisNum) // each time a number is guessed correctly it is then added to the guessed numbers list
     }
   }
+
   if (guessed_count > 0) {
-    instructionsEl.innerText = `You have guessed ${guessed_count} Numbers: ${guessed_list.toString}`
+    instructionsEl.innerText = (`You have guessed: ${guessed_count} Numbers correctly. Here's a list of the correct ones: ${guessed_list}`)
   }
 
 })
