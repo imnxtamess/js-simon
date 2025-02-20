9518dcec8093909c44b3a83191954b3a9c6d6b88
imnxtamess <davide.morisco.work@gmail.com>
2025-02-20 14:08:58 +0100

    add countdown limitation

--- js/script.js
+++ js/script.js
@@ -5,12 +5,17 @@ const numbersEl = document.getElementById("numbers-list")
 const formEl = document.getElementById("answers-form")
 
 
-let timer = 30;
+let timer = 5;
 
 // create countdown function
 
 const countdownId = setInterval(function () {
+
   timer--
   countdownEl.innerText = timer;
+
+  if (timer === 0) {
+    clearInterval(countdownId)
+  }
 }, 1000)
 
