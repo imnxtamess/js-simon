76c4249ef908b2754d0dd2583fcca23e4eb08646
imnxtamess <davide.morisco.work@gmail.com>
2025-02-20 16:00:59 +0100

    add guessed numbers counter logic

--- js/script.js
+++ js/script.js
@@ -4,6 +4,12 @@ const countdownEl = document.getElementById("countdown")
 const numbersListEl = document.getElementById("numbers-list")
 const formEl = document.getElementById("answers-form")
 const instructionsEl = document.getElementById("instructions")
+const inputNum1 = document.querySelector('#input-group>input:first-child')
+const inputNum2 = document.querySelector('#input-group>input:nth-child(2)')
+const inputNum3 = document.querySelector('#input-group>input:nth-child(3)')
+const inputNum4 = document.querySelector('#input-group>input:nth-child(4)')
+const inputNum5 = document.querySelector('#input-group>input:last-child')
+
 
 
 let timer = 3;
@@ -44,10 +50,40 @@ function pushNumsToPage() {
     thisNum = numList[i];
     numbersListEl.insertAdjacentHTML('beforeend', `<li>${thisNum}</li>`)
   }
+  return numList
 }
 
 pushNumsToPage()
 
+const ranNum1 = document.querySelector("#numbers-list>li:first-child")
+const ranNum2 = document.querySelector('#numbers-list>li:nth-child(2)')
+const ranNum3 = document.querySelector('#numbers-list>li:nth-child(3)')
+const ranNum4 = document.querySelector('#numbers-list>li:nth-child(4)')
+const ranNum5 = document.querySelector("#numbers-list>li:last-child")
+
+const ranNumList = [ranNum1.innerText, ranNum2.innerText, ranNum3.innerText, ranNum4.innerText, ranNum5.innerText]
+
+
+
+
+// save the input numbers into an array after the button gets clicked
+
+formEl.addEventListener('submit', function (e) {
+  e.preventDefault()
+  const inputArray = [];
+  inputArray.push(inputNum1.value, inputNum2.value, inputNum3.value, inputNum4.value, inputNum5.value)
+  console.log(inputArray);
+  let guessed_count = 0
+  for (let i = 0; i < inputArray.length; i++) {
+    thisNum = inputArray[i]
+    if (ranNumList.includes(thisNum)) {
+      guessed_count += 1
+      console.log(guessed_count);
+
+    }
+  }
+
+})
 
 
 
